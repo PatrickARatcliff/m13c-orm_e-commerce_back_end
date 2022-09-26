a54b8cba00d939cccaf07fca9344ed192428f320
PatrickARatcliff <pratcliff5@gmail.com>
2022-09-26 17:45:27 -0600

    format index.js

--- models/index.js
+++ models/index.js
@@ -14,9 +14,13 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE',
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag});
+Product.belongsToMany(Tag, { 
+  through: ProductTag
+});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag});
+Tag.belongsToMany(Product, { 
+  through: ProductTag
+});
 
 module.exports = {
   Product,
